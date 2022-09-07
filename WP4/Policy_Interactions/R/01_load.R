0a18c452673d1133b8becf8ed6f9345f6e69172f
Anna-Jorgensen <aejorge01@gmail.com>
2022-09-07 10:47:17 +0200

    load EUR-Lex search results

--- WP4/Policy_Interactions/R/01_load.R
+++ WP4/Policy_Interactions/R/01_load.R
@@ -4,7 +4,31 @@ rm(list = ls())
 
 
 # Load libraries ----------------------------------------------------------
-#library("")
 
+library("readr")
 
 # Load data ---------------------------------------------------------------
+
+# In eur-lex search ---------------
+
+# query term: marine protected area* (no parenthesis!!)
+
+# Resource Types downloaded from the search term:
+  # Directives: done
+  # Legislative acts: done - sent to email w/in next couple days...
+  # Regulation: done - sent to email w/in next couple days...
+  # Delegated regulation (del.reg): done 
+  # Delegated act: --> not found in the search
+  # National implementations --> not found in the search
+  # Decisions adopted by bodies created by international agreements: done 
+  # Treaties: done
+  # Convention: done
+  # other acts: done
+
+setwd("/Users/annajorgensen/Desktop/Anna's R code/mpa4sustainability/WP4/Policy_Interactions/data/raw_data/EUR-Lex_Search")
+
+EURLEX.file.list <- list.files(pattern='*.csv' )
+
+EURLEX.df <- read_csv(EURLEX.file.list, id = "resource.type")
+
+
