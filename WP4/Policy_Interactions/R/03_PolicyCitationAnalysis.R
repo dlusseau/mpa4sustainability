9f53bf4fe5d3a6e71b86c21ed481c6ffe23cfb18
Anna-Jorgensen <aejorge01@gmail.com>
2022-09-12 16:02:12 +0200

    working on cleaning interaction data. Very unfinished

--- WP4/Policy_Interactions/R/03_PolicyCitationAnalysis.R
+++ WP4/Policy_Interactions/R/03_PolicyCitationAnalysis.R
@@ -46,53 +46,81 @@ zero_cit <-
 citation_table.nozeros <- citation.table[!citation.table$Var1 %in% zero_cit$CELEX,]
 
 n_distinct(citation_table.nozeros$Var1)
-# 700 documents
-#1094-394 = 700 so he math adds up 
+# 705 documents
+n_distinct(citation.table$Var1)
+n_distinct(zero_cit$CELEX)
+#1104-399 = 705 so he math adds up 
 
-citation.table.nozeros <-
-  citation_table.nozeros  %>%
-  pivot_wider(
-    names_from = Var1,
-    values_from = Freq) %>%
-  column_to_rownames(.,  var = "Var2") 
+#filter out those that cite zero documents:
+citation_table.nozeros <- citation.table[!citation.table$Var1 %in% zero_cit$CELEX,]
 
-citation_matrix.nozeros <-
-  as.matrix(citation.table.nozeros)
 
-str(citation_matrix.nozeros)
-#  int [1:2919, 1:700] 0 0 0 0 0 0 0 0 0 0 ...
-#- attr(*, "dimnames")=List of 2
-#..$ : chr [1:2919] "11951K098" "11957A031" "11957A097" "11957A101" ...
-# the rownames are the citationcelexs
-
-#..$ : chr [1:700] "31972R0574" "31977D0415" "31977R2500" "31977R2714" ...
-# the column names are the pulled eurlex documents
-library("igraph")
-
-colnames(citation_matrix.nozeros) <- colnames(citation_matrix.nozeros) 
-rownames(citation_matrix.nozeros) <- rownames(citation_matrix.nozeros)
-
-network <-  graph_from_incidence_matrix(citation_matrix.nozeros)
-plot(network)
 
 citation_info <- document.key.df[document.key.df$celex %in% citation.table$Var2,]
 # O.K. we need to note that the all those cited are not legislation... 
 # so we need to filet those one out or not depending on if we only are interested in legislative documents...
 citation_info <-
   citation_info %>% 
-  distinct(celex, .keep_all = TRUE)
-
+  distinct(celex, .keep_all = TRUE) %>%
+  select(resource.type,celex,date,force) %>%
+  rename(CELEX = celex) %>%
+  mutate(pulled.from = "reference")
+  
 network.attributes <-
   mpa.policy.notext.df %>%
   select(resource.type,CELEX,date,force) %>%
   distinct(CELEX,.keep_all = TRUE) %>%
-  mutate(Document.pull = "eurlex.web")
+  mutate(pulled.from = "eurlex.web") %>%
+  rbind(.,citation_info)
+
+both.pulls <-
+  network.attributes %>%
+  group_by(CELEX) %>%
+  summarise(n=n()) %>%
+  filter(n>1)
+
+network.attributes.both <- network.attributes[network.attributes$CELEX %in% both.pulls$CELEX,]
+
+network.attributes.both <- 
+  network.attributes.both %>%
+  distinct(CELEX,.keep_all = TRUE) %>%
+  mutate(pulled.from= "both")
+
+network.attributes.notboth <- network.attributes[!network.attributes$CELEX %in% both.pulls$CELEX,]
+
+network.attributes.final <-
+  rbind(network.attributes.both,network.attributes.notboth)
 
 
+# now make a matrix with 0 referencing filtered out and non/legislation documents filtered out:
+citation_table.nozeros.leg <- citation_table.nozeros[citation_table.nozeros$Var2 %in% network.attributes.final$CELEX,]
 
 
+citation_table.nozeros.leg1 <-
+  citation_table.nozeros.leg  %>%
+  pivot_wider(
+    names_from = Var1,
+    values_from = Freq) %>%
+  column_to_rownames(.,  var = "Var2") 
 
-# Mentioned documents: 
+citation_matrix.nozeros.leg2 <-
+  as.matrix(citation_table.nozeros.leg1)
+
+str(citation_matrix.nozeros.leg2)
+# the rownames are the citationcelexs
+# the column names are the pulled eurlex documents
+
+
+library("igraph")
+
+colnames(citation_matrix.nozeros.leg2) <- colnames(citation_matrix.nozeros.leg2) 
+rownames(citation_matrix.nozeros.leg2) <- rownames(citation_matrix.nozeros.leg2)
+
+network <-  graph_from_incidence_matrix(citation_matrix.nozeros.leg2)
+plot(network)
+
+
+# Mentioned documents within EU MPA designations: 
 # habitats directive (31992L0043): https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:31992L0043 (92/43/EEC)
 # birds directive (32009L0147): https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:32009L0147
 # barcelona convention (21976A0216(01)): https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=celex%3A21976A0216%2801%29
@@ -100,8 +128,57 @@ network.attributes <-
 # cartagena convention (22002A0731(01)): https://eur-lex.europa.eu/legal-content/en/ALL/?uri=CELEX:22002A0731(01)
 # helcom (52021PC0534): https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:52021PC0534
 
+MPA.desigCELEX <- c("31992L0043",
+                    "32009L0147",
+                    "21976A0216(01)",
+                    "21998A0403(01)",
+                    "22002A0731(01)",
+                    "52021PC0534")
+
+MPA.desigName <- c("habitats directive",
+                    "birds directive",
+                    "barcelona convention",
+                    "ospar",
+                    "cartagena convention",
+                    "helcom")
+
+Desig.Celex <- tibble(
+  CELEX = MPA.desigCELEX,
+  Name = MPA.desigName
+)
 
 
+testing <- 
+  citation_table.nozeros.leg[citation_table.nozeros.leg$Var2 %in% Desig.Celex$CELEX,]
 
+testing <- 
+  testing %>%
+  group_by(Var1) %>%
+  mutate(total = sum(Freq)) %>%
+  filter(total>0) %>%
+  select(-total)
+
+testing <-
+  testing  %>%
+  pivot_wider(
+    names_from = Var1,
+    values_from = Freq) %>%
+  column_to_rownames(.,  var = "Var2") 
+
+testing2 <-
+  as.matrix(testing)
+
+str(testing2)
+# the rownames are the citationcelexs
+# the column names are the pulled eurlex documents
+
+
+library("igraph")
+
+colnames(testing2) <- colnames(testing2) 
+rownames(testing2) <- rownames(testing2)
+
+network <-  graph_from_incidence_matrix(testing2)
+plot(network)
 
 
