257b02a55f828ba9d5b4c08647c3e8f6ad45e39b
Anna Elizabeth Jorgensen <aeljor@dtu.dk>
2022-09-14 12:10:00 +0200

    building doc-citation indencies matrix and associated network

--- WP4/Policy_Interactions/R/03_PolicyCitationAnalysis.R
+++ WP4/Policy_Interactions/R/03_PolicyCitationAnalysis.R
@@ -18,66 +18,79 @@ document.key.df <- read.csv(file = "WP4/Policy_Interactions/data/01_SPARQL.key.d
 
 # Exploring document referencing ------------------------------------------
 
-Dist.citations <-
+# (1) We want a matrix of the MPA documents (rows) v.s. documents they cite (columns)
+
+# Note: the citation data from eurlex does not provide how many times the document was referenced/cited within the document.
+# Only that the document was cited (thus citation frequency is 1)
+
+Doc.citations <-
   mpa.policy.notext.df %>%
-  mutate(date = as.Date(date)) %>%
-  mutate(year = year(date)) %>%
-  distinct(CELEX,citationcelex)
+  filter(!is.na(work)) %>% #filtering out the document that is an issue (all data missing) --> reference 02.Rscript
+  distinct(CELEX,citationcelex) # make sure no duplicate rows bc of multiple labeles/themes
 
-citation.table <- as.data.frame(table(Dist.citations$CELEX,Dist.citations$citationcelex))
+citation.table <- as.data.frame(table(Doc.citations$CELEX,Doc.citations$citationcelex))
 
-# Here we will create an incidence matrix:
-citation_table <-
+citation.table <-
+  citation.table %>%
+  rename("Document"="Var1",
+         "Citation"="Var2")
+
+# Lets create an incidence matrix:
+citation_matrix <-
   citation.table  %>%
   pivot_wider(
-    names_from = Var1,
+    names_from = Citation,
     values_from = Freq) %>%
-  column_to_rownames(.,  var = "Var2") 
+  column_to_rownames(.,  var = "Document") 
   
 zero_cit <-
-  citation_table %>%
-  colSums() %>%
+  citation_matrix %>%
+  rowSums() %>%
   as.data.frame() %>%
   rename("cit.nmbr" = ".") %>%
   filter(cit.nmbr == 0) %>%
   rownames_to_column(var = "CELEX")
+# 5 documents have no citations
 
-#filter out those that cite zero documents:
-citation_table.nozeros <- citation.table[!citation.table$Var1 %in% zero_cit$CELEX,]
+#Eurlex data/attributes about the citations
+citation_info <- document.key.df[document.key.df$celex %in% citation.table$Citation,]
 
-n_distinct(citation_table.nozeros$Var1)
-# 705 documents
-n_distinct(citation.table$Var1)
-n_distinct(zero_cit$CELEX)
-#1104-399 = 705 so he math adds up 
-
-#filter out those that cite zero documents:
-citation_table.nozeros <- citation.table[!citation.table$Var1 %in% zero_cit$CELEX,]
-
-
-
-citation_info <- document.key.df[document.key.df$celex %in% citation.table$Var2,]
-# O.K. we need to note that the all those cited are not legislation... 
-# so we need to filet those one out or not depending on if we only are interested in legislative documents...
 citation_info <-
   citation_info %>% 
   distinct(celex, .keep_all = TRUE) %>%
   select(resource.type,celex,date,force) %>%
   rename(CELEX = celex) %>%
   mutate(pulled.from = "reference")
+# NOTE: there is a total of 208 citation documents but only 134 have eurolex data associated... this is bc our key is only leg. documents...
+# so depending on if we only are interested in legislative documents or not we should or shouldnt remove them
+# for now keeping all of the citations regardless of the type of document 
+# I will add them into this DF but will put resource.type as non-leg and NA for other fields. 
+
+citation_info1 <- 
+  citation.table %>%
+  distinct(Citation) %>%
+  anti_join(.,citation_info, by=c("Citation"="CELEX"))%>%
+  mutate(date = NA,
+         force = NA,
+         resource.type = "OTHER", 
+         pulled.from = "reference") %>% 
+  rename("CELEX" = "Citation") %>%
+  rbind(.,citation_info) #combine non.leg with the leg data 
+#hmm seems like celex numbers that start with a 5 are here but opinions are 5 aswell....
   
 network.attributes <-
   mpa.policy.notext.df %>%
   select(resource.type,CELEX,date,force) %>%
   distinct(CELEX,.keep_all = TRUE) %>%
   mutate(pulled.from = "eurlex.web") %>%
-  rbind(.,citation_info)
+  rbind(.,citation_info1)
 
 both.pulls <-
   network.attributes %>%
   group_by(CELEX) %>%
   summarise(n=n()) %>%
-  filter(n>1)
+  filter(n>1) 
+# 4 documents were pulled as an MPA leg and cited within others
 
 network.attributes.both <- network.attributes[network.attributes$CELEX %in% both.pulls$CELEX,]
 
@@ -91,33 +104,29 @@ network.attributes.notboth <- network.attributes[!network.attributes$CELEX %in% 
 network.attributes.final <-
   rbind(network.attributes.both,network.attributes.notboth)
 
+network.attributes.final <-
+  network.attributes.final %>%
+  select(CELEX,resource.type,date,force,pulled.from)
 
-# now make a matrix with 0 referencing filtered out and non/legislation documents filtered out:
-citation_table.nozeros.leg <- citation_table.nozeros[citation_table.nozeros$Var2 %in% network.attributes.final$CELEX,]
+network <- graph_from_data_frame(d=citation.table, vertices = network.attributes.final, directed = FALSE)
 
+V(network)$color <- V(network)$pulled.from
 
-citation_table.nozeros.leg1 <-
-  citation_table.nozeros.leg  %>%
-  pivot_wider(
-    names_from = Var1,
-    values_from = Freq) %>%
-  column_to_rownames(.,  var = "Var2") 
-
-citation_matrix.nozeros.leg2 <-
-  as.matrix(citation_table.nozeros.leg1)
-
-str(citation_matrix.nozeros.leg2)
-# the rownames are the citationcelexs
-# the column names are the pulled eurlex documents
-
+plot(network)
+#hmm this looks wrong
 
 library("igraph")
 
-colnames(citation_matrix.nozeros.leg2) <- colnames(citation_matrix.nozeros.leg2) 
-rownames(citation_matrix.nozeros.leg2) <- rownames(citation_matrix.nozeros.leg2)
+colnames(citation_matrix) <- colnames(citation_matrix) 
+rownames(citation_matrix) <- rownames(citation_matrix)
 
-network <-  graph_from_incidence_matrix(citation_matrix.nozeros.leg2)
-plot(network)
+network <-  graph_from_incidence_matrix(citation_matrix, 
+                                        directed = TRUE,
+                                        mode = c("out"))
+plot(network,
+     vertex.size=10,
+     vertex.label.cex=.5,
+     )
 
 
 # Mentioned documents within EU MPA designations: 
@@ -180,5 +189,19 @@ rownames(testing2) <- rownames(testing2)
 
 network <-  graph_from_incidence_matrix(testing2)
 plot(network)
+
+
+# Archival code ---------------------------------------
+
+# (Maybe we want to do this but for now will keep them in)
+#filter out those that cite zero documents:
+#citation_table.nozeros <- citation.table[!citation.table$Var1 %in% zero_cit$CELEX,]
+#n_distinct(citation_table.nozeros$Var1)
+#n_distinct(citation.table$Var1)
+#n_distinct(zero_cit$CELEX)
+#filter out those that cite zero documents:
+#citation_table.nozeros <- citation.table[!citation.table$Var1 %in% zero_cit$CELEX,]
+
+
 
 
