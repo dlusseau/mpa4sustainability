42a38676104d1c2490cfa730af91f970ce6a3d8f
Anna Elizabeth Jorgensen <aeljor@dtu.dk>
2022-09-23 13:46:16 +0200

    finished second order citation and term network for MPA term query documents.

--- WP4/Policy_Interactions/R/04_MPAterm_query.R
+++ WP4/Policy_Interactions/R/04_MPAterm_query.R
@@ -7,6 +7,8 @@ rm(list = ls())
 library("igraph")
 library("widyr")
 library("dplyr")
+library("tibble")
+library("tidyr")
 
 # Define functions --------------------------------------------------------
 
@@ -149,7 +151,7 @@ plot(network,
      edge.arrow.width=1,
      rescale=F,
      layout=l,# trying this layout based on pdf above...
-   #  edge.curved=.1
+     #edge.curved=.1
 )
 # blue are documents referenced within text
 # green are those pulled from out MPA eurlex search
@@ -188,6 +190,63 @@ Doc.citations.2 <-
 
 citation.info2 <-  leg.citation_info2
 
+
+network.attributes.final2 <-
+  citation.info2 %>%
+  select(CELEX,resource.type,date,force,pulled.from) %>%
+  mutate(color = 
+           case_when(
+             pulled.from == "reference2" ~ "purple" )) %>%
+  mutate(shape = 
+           case_when(
+             resource.type == "DIR" ~ "circle",
+             resource.type == "REG" ~ "circle",
+             resource.type == "DEC" ~ "circle",
+             resource.type == "RECO" ~ "circle",
+             resource.type == "OPIN" ~ "circle",
+             resource.type == "OTHER" ~ "square"  ))
+
+n_distinct(network.attributes.final2$CELEX)
+#935
+
+Doc.citations3 <- rbind(MPA.citations,Doc.citations.2)
+
+network.attributes.final3 <- rbind(network.attributes.final,network.attributes.final2)
+
+network.attributes.final3 <- 
+  network.attributes.final3 %>%
+  distinct(CELEX, .keep_all = TRUE)
+
+n_distinct(Doc.citations3$to)
+# 312
+n_distinct(Doc.citations3$from)
+# 1065
+312+1065
+#1377
+docs <- unique(Doc.citations3$to)
+cit <-  unique(Doc.citations3$from)
+xx <- as.data.frame(c(docs,cit))
+xx <- distinct(xx)
+
+network <- graph.data.frame(d=Doc.citations3, directed = TRUE, vertices = network.attributes.final3)
+print(network, e=TRUE, v=TRUE)
+
+l <- layout.fruchterman.reingold(network)
+l <- layout.norm(l, ymin=-1, ymax=1, xmin=-1, xmax=1)
+
+plot(network,
+     edge.width=.5,
+     vertex.size=2,
+     vertex.label=NA,
+     vertex.label.cex=1,
+     edge.arrow.size=.5,
+     edge.arrow.width=1,
+     layout=l*1.2,
+     edge.curved=.1,
+     vertex.label.dist = 0.5,
+)
+
+
 # Exploring Eurovoc terms -----------------------------------------------
 
 # Eurovoc Term Co-occurrences: 
@@ -195,14 +254,7 @@ cleaned.labels <-
   EU.mpa.termsearch.data %>%
   distinct(CELEX,labels, .keep_all = TRUE)
 
-term.table <- as.data.frame(table(cleaned.labels$CELEX,cleaned.labels$labels))
-
-term.table <- 
-  term.table %>%
-  rename(CELEX = Var1,
-         label = Var2)
-
-head(mpa.policy.notext.df.cleaned)
+n_distinct(cleaned.labels$MT)
 
 label.pairs <- 
   cleaned.labels %>%
@@ -215,21 +267,9 @@ label.pairs$all<-apply(apply(cbind(as.character(label.pairs$item1),as.character(
 #duplicated should work on this
 label.pairs.sub<-label.pairs[!duplicated(label.pairs$all),]
 
-#maybe a network plot is a better visualization for this data:
-term.pairs_matrix <-
-  label.pairs.sub  %>%
-  select(-all) %>%
-  pivot_wider(
-    names_from = item1,
-    values_from = n)%>%
-  column_to_rownames(.,var = "item2")
-
-term.pairs_matrix[is.na(term.pairs_matrix)] <- 0
-term.pairs_matrix <- as.matrix(term.pairs_matrix)
-
 term.pairs<- 
   label.pairs.sub %>%
-  select(-all)
+  select(-all) 
 
 attributes1<- 
   label.pairs.sub %>%
@@ -281,15 +321,14 @@ final.attributes <-
   select(-c("sum1","sum2")) %>%
   left_join(.,attributes3, by = c("item1"="labels"))
 
+n_distinct(final.attributes$MT)
 
-n <-label.pairs.sub$n*1.15
-
-network <- graph_from_data_frame(d=term.pairs, vertices = final.attributes1, directed = FALSE)
+network <- graph_from_data_frame(d=term.pairs, vertices = final.attributes, directed = FALSE)
+class(network)
 
 #very helpful document for network vizualizations 
 #http://www.kateto.net/wp-content/uploads/2015/06/Polnet%202015%20Network%20Viz%20Tutorial%20-%20Ognyanova.pdf
 
-V(network)$color <- V(network)$color
 
 l <- layout.fruchterman.reingold(network)
 l <- layout.norm(l, ymin=-1, ymax=1, xmin=-1, xmax=1)
@@ -306,253 +345,66 @@ plot(network,
      # trying this layout based on pdf above...
 )
 
+# O.K. so the network viz is more legable 
+# I will only plot those that are the median or above edges
 
-summary(final.attributes$total.count)
-# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 1.00    6.00    9.50   19.41   21.00  383.00 
+I1 <-
+  term.pairs %>%
+  group_by(item1) %>%
+  summarise(n=n())
+I2 <-
+  term.pairs %>%
+  group_by(item2) %>%
+  summarise(n=n())
 
-final.attributes1 <-
-  final.attributes %>%
-  mutate(count.grps = 
-           case_when(
-             total.count == 1 ~ .1,
-             total.count <= 6 ~ .6,
-             total.count <= 19.41 ~ 1.1,
-             total.count <= 21 ~ 1.6,
-             total.count < 383 ~ 2.1.25,
-             total.count == 383 ~ 4))
+I3 <- full_join(I1,I2, by=c("item1"="item2")) %>%
+  mutate(n.x = replace_na(n.x,0),
+         n.y = replace_na(n.y,0))%>%
+  mutate(edge.number = n.x+n.y )
+
+summary(I3$edge.number)
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#   1.00    6.00    9.00   13.07   15.00  175.00
+
+#this tutorial was helpful for this vizualization
+#https://tm4ss.github.io/docs/Tutorial_5_Co-occurrence.html#4_Visualization_of_co-occurrence
+#https://kateto.net/wp-content/uploads/2016/06/Polnet%202016%20R%20Network%20Visualization%20Workshop.pdf
+edges.remove <- V(network)[degree(network)<13]
+degree(network)
+graphNetwork <-  igraph::delete.vertices(network,edges.remove) 
+degree(graphNetwork)
 
 
-## Archival for now ==============================================================
+library("viridis")   
 
-# First lets search for the specific documents in mention:
+colors <- inferno(34)
+colors <- colors[-1:-5]
+V(graphNetwork)$color <- colors[as.numeric(as.factor(V(graphNetwork)$MT))]
 
-# Mentioned documents within EU MPA designations: 
-# habitats directive (31992L0043): https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:31992L0043 (92/43/EEC)
-# birds directive (32009L0147): https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:32009L0147
-# barcelona convention (21976A0216(01)): https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=celex%3A21976A0216%2801%29
-# ospar (21998A0403(01)): https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=celex%3A21998A0403%2801%29
-# cartagena convention (22002A0731(01)): https://eur-lex.europa.eu/legal-content/en/ALL/?uri=CELEX:22002A0731(01)
-# helcom (52021PC0534): https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:52021PC0534
+dist <- seq(-.025,0.25, by=.0024)
+dist <- rep(c(0.175, -0.175), length.out = 113)
+#try to jitter the labels a little to avoid overlap 
+V(graphNetwork)$dist <- dist[as.numeric(as.factor(V(graphNetwork)$name))]
 
-MPA.desigCELEX <- c("31992L0043",
-                    "32009L0147",
-                    "21976A0216(01)",
-                    "21998A0403(01)",
-                    "22002A0731(01)",
-                    "52021PC0534")
 
-MPA.desigName <- c("habitats directive",
-                   "birds directive",
-                   "barcelona convention",
-                   "ospar",
-                   "cartagena convention",
-                   "helcom")
+l2 <- layout.fruchterman.reingold(graphNetwork)
 
-Desig.Celex <- tibble(
-  CELEX = MPA.desigCELEX,
-  Name = MPA.desigName
+plot(graphNetwork,
+     edge.width=V(graphNetwork)$n,
+     edge.color=adjustcolor("gray", alpha.f = .25),
+     vertex.size=2,
+     vertex.label.cex=(degree(graphNetwork)/sum(degree(graphNetwork))*100), # label size is equiv. to percent of edges associated to the word out of total edges
+     vertex.label.color=V(graphNetwork)$color,
+     vertex.shape="none",
+     rescale = TRUE,
+     ylim=c(-.8,.85),xlim=c(-.85,.9),
+     layout = l2,
+     vertex.label.family = "sans",
+     vertex.label.dist = V(graphNetwork)$dist
+     # trying this layout based on pdf above...
 )
 
-# lets pull out the documents that specifically reference these above
-doc.referenced <-
-  citation.table.xx %>% # to-from citation table built above
-  filter(from %in% Desig.Celex$CELEX) %>%
-  left_join(.,Desig.Celex, by=c("from"="CELEX")) %>%
-  select(to,from,Name) %>%
-  #mutate(Name. = Name) %>%
-  rename("from.name" = "Name")
-# select(to,from,Name)
 
 
-# total search terms:
-search_terms <- c("ramsar site", 
-                  "wetland of international importance",
-                  "world heritage site (natural or mixed)", 
-                  "unesco-mab biosphere reserve",
-                  "specially protected areas of mediterranean importance",
-                  "barcelona convention",
-                  "sites of community importance",
-                  "habitats directive",
-                  "special areas of conservation",
-                  "habitats directive",
-                  "special protection area",
-                  "birds directive",
-                  "baltic sea protected area",
-                  "helcom",                                          
-                  # "marine protected area", --> removing this one bc this was the original search term...
-                  "ospar",
-                  "specially protected area",
-                  "cartagena convention")
-# add a ? at the end 
 
-text.ref.list<-list()
-
-for (i in 1:length(search_terms)) {  
-  text.ref.list[[i]]<- 
-    MPA.textdata %>%
-    select(-c(title,text)) %>%
-    mutate(references = str_extract_all(total.text, search_terms[[i]])) %>%
-    select(-c(total.text))
-}
-
-# Lets give each list the name based on the resource type: 
-text.ref.list <- structure(text.ref.list, names=search_terms)
-
-text.df <- bind_rows(text.ref.list, .id = "search.term" )
-
-referenced.df <- unnest(text.df, references) 
-# here number of rows indicated how many times the search term was written within the document 
-# if that is unimportant to us then we should remve duplicate rows.
-# Documents where the search terms appeared: 
-#"32008L0056" --> marine strategy framework directive
-#"32013L0030" -->  safety of offshore oil and gas operations and amending Directive
-#"32013R1380" --> Common Fisheries Policy, amending Council Regulations
-#"31984D0132"--> Protocol concerning Mediterranean specially protected areas
-
-# O.K. if e combine the document ref and the term ref then the number of times referenced doesnt matter:
-referenced.df1 <- 
-  referenced.df %>%
-  distinct(.keep_all = TRUE) %>%
-  rename("to"="CELEX") %>%
-  mutate(from="celex.unk") %>%
-  select(to,from,references) %>%
-  rename("from.name"="references")
-
-total.MPA.ref <- 
-  rbind(referenced.df1,doc.referenced) %>%
-  mutate(from.name = str_trim(from.name, side = "both"))
-
-
-#join the MPA ref to the actual MPAs
-
-MPA.links <- 
-  mpa.DES.key1 %>%
-  left_join(.,total.MPA.ref, by = c("term"="from.name")) %>%
-  select(to,from,term,mpa)
-
-n_distinct(mpa.DES.key1$mpa)
-#3604
-n_distinct(MPA.links$mpa)
-#3604
-
-# 1,373 MPAs dont have a document reference these are:
-not.linked <-
-  MPA.links %>%
-  filter(is.na(to)) %>%
-  distinct(mpa)
-#this is bc 12 search terms never popped up in any documents:
-x <- MPA.links %>%
-  filter(is.na(to))
-
-unique(x$term)
-#[1] "ramsar site"                                          
-#[2] "wetland of international importance"                  
-#[3] "natural or mixed"                                     
-#[4] "world heritage site"                                  
-#[5] "unesco-mab biosphere reserve"                         
-#[6] "barcelona convention"                                 
-#[7] "specially protected areas of mediterranean importance"
-#[8] "sites of community importance"                        
-#[9] "helcom"                                               
-#[10] "baltic sea protected area"                            
-#[11] "marine protected area"                                
-#[12] "cartagena convention"
-
-# OK lets try to make a document-mpa link network:
-
-MPA.links.table <- 
-  MPA.links %>%
-  filter(!is.na(to)) %>% # filter out mpas that dont link to documents
-  select(-from) %>% 
-  rename("from" = "mpa") %>%
-  select(to, from, term) %>% # note: to is the celex of the document, from is the mpa id number
-  distinct(to, from, .keep_all = TRUE) #duplicated links bc search terms overlap... do getting rid of that
-
-n_distinct(MPA.links.table$from)
-#3160 
-n_distinct(MPA.links.table$to)
-#7
-
-celex.info <-
-  network.attributes.final %>%
-  filter(CELEX %in% MPA.links.table$to) %>%
-  rename("id" = "CELEX") %>%
-  select(id) %>%
-  mutate(data.type = "EU.Leg")
-
-
-Net.attributes <-
-  mpa.DES.key %>%
-  filter(mpa %in% MPA.links.table$from ) %>% # 3604-1373 =2231 obj. dim. add up
-  mutate(data.type = "MPA") %>%
-  rename("id" = "mpa") %>%
-  select(id,data.type) %>%
-  rbind(.,celex.info)
-
-
-network <- graph_from_data_frame(d=MPA.links.table, directed = FALSE, vertices = Net.attributes)
-print(network, e=TRUE, v=TRUE)
-l <- layout.fruchterman.reingold(network)
-
-l <- layout.fruchterman.reingold(network)
-l <- layout.norm(l, ymin=-1, ymax=1, xmin=-1, xmax=1)
-
-plot(network,
-     edge.width=.5,
-     vertex.size=5,
-     vertex.label=NA,
-     #  vertex.label.cex=.75,
-     edge.arrow.size=.75,
-     edge.arrow.width=.75,
-     rescale=F,
-     layout=l*1.1 # trying this layout based on pdf above...
-)
-
-# ok instead maybe a better approach is document to designation type?
-# instead of the actual mpa id...
-# will try this out below
-
-MPA.links.table2 <- 
-  MPA.links %>%
-  filter(!is.na(to)) %>% # filter out mpas that dont link to documents
-  select(-from) %>% 
-  rename("from" = "mpa") %>%
-  select(to, from, term) %>% # note: to is the celex of the document, from is the mpa id number
-  distinct(to,term)%>% 
-  rename("from" = "term")
-
-n_distinct(MPA.links.table2$to)
-#7 celex
-n_distinct(MPA.links.table2$from)
-# 6 mps types 
-
-Net.attributes2 <-
-  MPA.links.table2 %>%
-  distinct(from)%>% 
-  mutate(data.type = "MPA.term") %>%
-  rename("id" = "from") %>%
-  select(id,data.type) %>%
-  rbind(.,celex.info) %>%
-  mutate(color = 
-           case_when(
-             data.type == "MPA.term" ~ "#0cb702",
-             data.type == "EU.Leg" ~ "#f8766d" ))
-
-network2 <- graph_from_data_frame(d=MPA.links.table2, directed = FALSE, vertices = Net.attributes2)
-print(network2, e=TRUE, v=TRUE)
-
-l <- layout_nicely(network2)
-l <- layout.norm(l, ymin=-1, ymax=1, xmin=-1, xmax=1)
-
-plot(network2,
-     edge.width=.5,
-     vertex.size=10,
-     vertex.label=1,
-     vertex.label.cex=.75,
-     edge.arrow.size=.75,
-     edge.arrow.width=.75,
-     rescale=F,
-     layout=l # trying this layout based on pdf above...
-)
 
