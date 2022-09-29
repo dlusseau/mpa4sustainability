b53c008b79709556d27e0262790e2cd4f93ebf8a
Anna Elizabeth Jorgensen <aeljor@dtu.dk>
2022-09-29 16:43:07 +0200

    double checking code, object dimention, fixing graphics, and additional comments when necessary. not complete stopped at line 321

--- WP4/Policy_Interactions/R/04_MPAterm_query.R
+++ WP4/Policy_Interactions/R/04_MPAterm_query.R
@@ -32,15 +32,23 @@ document.key.df <- read.csv(file = "WP4/Policy_Interactions/data/01_SPARQL.key.d
 # lets join the new mpa search documents to their associated document data
 EU.mpa.termsearch.data <-
   EU.mpa.termsearch %>%
+  filter(CELEX != "32021R0092") %>% #removing this celex for the same reasoning as in 03 Rscript
   left_join(., document.key.df, by = c("CELEX"="celex", "resource.type")) %>%
   select(-work,-type) # we dont need this info anymore
 
-# check
-n_distinct(EU.mpa.termsearch$CELEX)
-#[1] 180
+
 n_distinct(EU.mpa.termsearch.data$CELEX)
-#[1] 180
-# The check looks good but remember the dim are now larger due to some documents having multiple terms-labels etc. 
+#[1] 179
+# The remember the dim are now larger due to some documents having multiple terms-labels etc. 
+
+EU.mpa.termsearch.data %>%
+  group_by(resource.type) %>%
+  summarise(n = n_distinct(CELEX))
+# DEC              41
+# DIR              16
+# OPIN             83
+# RECO              3
+# REG              36
 
 # how many unique label terms?
 n_distinct(EU.mpa.termsearch.data$labels)
@@ -81,6 +89,7 @@ MPA.citations <-
   filter(citationcelex %in% leg.citation_info$CELEX) 
 
 citation.info <-  rbind(leg.citation_info) #combine non.leg with the leg data 
+
 
 network.attributes <-
   EU.mpa.termsearch.data %>%
@@ -128,34 +137,105 @@ network.attributes.final <-
              resource.type == "OTHER" ~ "square"  ))
 
 n_distinct(network.attributes.final$CELEX)
-# 387
+# 381
 
 MPA.citations <-
   MPA.citations %>%
   rename("to"="CELEX",
          "from"="citationcelex")
 
+n_distinct(MPA.citations$to)
+# 85
+
+docs <- unique(MPA.citations$to)
+cit <-  unique(MPA.citations$from)
+xx <- as.data.frame(c(docs,cit))
+xx <- distinct(xx) #381 documents
+# ok vertices df and attribute df dim are the same :) 
+
 network <- graph_from_data_frame(d=MPA.citations, directed = TRUE, vertices = network.attributes.final)
 print(network, e=TRUE, v=TRUE)
 
 
 l <- layout.fruchterman.reingold(network)
-l <- layout.norm(l, ymin=-1, ymax=1, xmin=-1, xmax=1)
+#l <- layout.norm(l, ymin=-1, ymax=1, xmin=-1, xmax=1)
+
+labels <- network.attributes.final[1:21,1]
+
+labels2 <- rep(NA,time=360)
+labels3 <- c(labels,labels2)
+V(network)$label <- labels3 
+
 
 plot(network,
      edge.width=.5,
-     vertex.size=3,
-     vertex.label=NA,
-     vertex.label.cex=1,
+     vertex.size=3.5,
+     vertex.label.family = "sans",
+ #  vertex.label=NA,
+     vertex.label.cex=.765,
+     vertex.label.color = "black",
      edge.arrow.size=.5,
      edge.arrow.width=1,
-     rescale=F,
+     rescale=TRUE,
      layout=l,# trying this layout based on pdf above...
      #edge.curved=.1
 )
+legend(x=-1.7,y=1.2,c("32008L0056: Marine Strategy Framework Directive",
+                        "32000L0060: Directive a framework for Community action in the field of water policy",
+                        "31992L0043: Habitats Directive" ,
+                        "32009L0147: Birds Directive" ,
+                        "32008L0099: Directive on the protection of the environment through criminal law"  ,
+                        "32011L0092: Directive on the assessment of the effects of certain public and private projects on the environment", 
+                        "32009L0031: Directive on the geological storage of carbon dioxide & amending CD 85/337/EEC, EP & CD 2000/60/EC, 2001/80/EC, 2004/35/EC, 2006/12/EC, 2008/1/EC & Reg. (EC) No 1013/2006",  
+                        "32006R1967: Reg. concerning management measures for the sustainable exploitation of fishery resources in the Mediterranean Sea, amending Reg. (EEC) No 2847/93 & repealing Reg. (EC) No 1626/94" ,
+                        "32004R0724: Reg. amending Reg. (EC) No 1406/2002 establishing a European Maritime Safety Agency" ,
+                        "32010R1089: Reg. implementing Directive 2007/2/EC of the European Parliament and of the Council as regards interoperability of spatial data sets and services", 
+                        "31997R0338: Reg. on the protection of species of wild fauna and flora by regulating trade therein"
+                       ),
+       cex=.65,
+       ncol=1,
+       col="#777777",
+       bty="n", # no box around the legen 
+       
+)
+legend(x=-1.7,y=-1,c("32013R1380: Reg. on the CFP, amending CR (EC) No 1954/2003 and 1224/2009 and repealing CR (EC) No 2371/2002 and 639/2004 and CD 2004/585/ECs",
+                      "32014R0508: Reg.on the European Maritime and Fisheries Fund and repealing CR (EC) No 2328/2003, No 861/2006, No 1198/2006 and No 791/2007 and Reg. (EU) No 1255/2011",
+                      "31999D0800: Barcelona Convention" , 
+                      "31999D0801: Dec. accepting amendments to the Protocol for the protection of the Mediterranean Sea against pollution from land-based sources (Barcelona Convention)", 
+                      "32013D0005: Dec. on the accession of the EU to the Protocol for the Protection of the Mediterranean Sea against pollution resulting from exploration and exploitation of the continental shelf and the seabed and its subsoil",
+                      "32009D0089: Dec. on the signing of the Protocol on Integrated Coastal Zone Management in the Mediterranean to the Convention for the Protection of the Marine Environment and the Coastal Region of the Mediterranean ",  
+                      "32002D1600: Dec. laying down the Sixth Community Environment Action Programme",  
+                      "32000D0340: Dec. approval of the new Annex V to the Convention for the Protection of the Marine Environment of the North-East Atlantic on the protection and conservation of the ecosystems and biological diversity of the maritime area",
+                      "52018AE2960: Opin. of the European Economic and Social Committee on ‘Proposal for a Regulation of the European Parliament and of the Council on the alignment of reporting obligations in the field of environment policy",
+                      "52017AE2820: Opin. of the European Economic and Social Committee on Commission Notice on Access to Justice in Environmental Matters"
+),
+cex=.65,
+ncol=1,
+col="#777777", 
+bty="n", # no box around the legen 
+
+)
+
+
+
+legend(x=-1.6,y=-.8,c("Both (result & citation)",
+                     "Search result",
+                     "First order citation"),
+       pch=21,
+       col="#777777", 
+       pt.bg=unique(V(network)$color), 
+       pt.cex=2, 
+       cex=1, 
+       bty="n", # no box around the legen 
+       ncol=1)
+
 # blue are documents referenced within text
 # green are those pulled from out MPA eurlex search
 # red/pink are those that were pulled in the MPA search and also referenced within other documents pulled
+edges <- degree(network)
+sum(edges)
+
+V(network)
 
 # Second order citations ---------------------------------------------------------------------------
 
@@ -213,6 +293,46 @@ Doc.citations3 <- rbind(MPA.citations,Doc.citations.2)
 
 network.attributes.final3 <- rbind(network.attributes.final,network.attributes.final2)
 
+# HERE IS WHERE YOU NEED TO INSERT THE NEW CODE RESOLVES THE THIRD REFERENCE ISSUE IT IS IN RSCRIPT 03.
+
+# some of the citations are both second and first order ciatations so lets make a new label... (color is orange and the name is reference 3)
+both.cit2 <-
+  network.attributes.final3 %>%
+  group_by(CELEX) %>%
+  mutate(n=n()) %>%
+  filter(n>1) %>% 
+  filter(!CELEX %in% both.pulls$CELEX) %>% #380-32 = 348
+  filter(CELEX != "32010D0631" &
+           CELEX != "32013D1386" &
+           CELEX != "31998D2179") %>% # this three are now a both so in total there are 19 docs that will have duplicates later. 
+  mutate(pulled.from = "reference3") %>%
+  mutate(color = 
+           case_when(
+             pulled.from == "reference3" ~ "orange" )) %>%
+  distinct()%>%
+  select(-n)
+
+network.attributes.final4 <- network.attributes.final3[!network.attributes.final3$CELEX %in% both.cit2$CELEX,]
+# 1316-342 = 974 math check add up :)
+
+network.attributes.final4 <- rbind(network.attributes.final4,both.cit2)
+# 974+171 = 1145 
+
+# stopped here: need to change all the celex info starting tomorrow
+network.attributes.final4 <- 
+  network.attributes.final4 %>%
+  mutate(pulled.from = case_when(CELEX == "32013D1386" ~ "both", # change this one to both since it is second order referenced. 
+                                 TRUE ~ pulled.from)) %>%
+  mutate(color = case_when(CELEX == "32013D1386" ~ "#f8766d", 
+                           TRUE ~ color)) %>%
+  mutate(remove = case_when(CELEX == "32008L0056" & pulled.from == "reference2" ~ "remove", # 
+                            CELEX == "32013R1380" & pulled.from == "reference2" ~ "remove",
+                            CELEX == "32014R0508" & pulled.from == "reference2" ~ "remove",
+                            TRUE ~ "keep")) %>%
+  filter(remove == "keep") %>%
+  distinct(CELEX, .keep_all = TRUE) # now remove the double 32013D1386
+
+#--------
 network.attributes.final3 <- 
   network.attributes.final3 %>%
   distinct(CELEX, .keep_all = TRUE)
@@ -255,6 +375,9 @@ cleaned.labels <-
   distinct(CELEX,labels, .keep_all = TRUE)
 
 n_distinct(cleaned.labels$MT)
+# 70 themes
+n_distinct(cleaned.labels$labels)
+# 403 terms
 
 label.pairs <- 
   cleaned.labels %>%
@@ -270,6 +393,8 @@ label.pairs.sub<-label.pairs[!duplicated(label.pairs$all),]
 term.pairs<- 
   label.pairs.sub %>%
   select(-all) 
+
+summary(term.pairs)
 
 attributes1<- 
   label.pairs.sub %>%
@@ -387,7 +512,6 @@ dist <- rep(c(0.18, -0.18), length.out = 226)
 #try to jitter the labels a little to avoid overlap 
 V(graphNetwork)$dist <- dist[as.numeric(as.factor(V(graphNetwork)$name))]
 
-n <-label.pairs.sub$n*1.15
 
 l2 <- layout.fruchterman.reingold(graphNetwork)
 
@@ -406,6 +530,21 @@ plot(graphNetwork,
      # trying this layout based on pdf above...
 )
 
+legend(x=.45,y=-.15,unique(V(graphNetwork)$MT)[-27], 
+       pch=21,
+       col="#777777", 
+       pt.bg=unique(V(graphNetwork)$color), 
+       pt.cex=2, 
+       cex=1, 
+       bty="n", # no box around the legen 
+       ncol=2)
+
+
+
+edges <- degree(graphNetwork)
+sum(edges)
+
+V(graphNetwork)
 
 
 
