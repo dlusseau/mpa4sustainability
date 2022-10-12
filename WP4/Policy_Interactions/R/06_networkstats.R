d7a11eea6baebc2b78b4a407bf5397a28202c6d9
Anna Elizabeth Jorgensen <aeljor@dtu.dk>
2022-10-12 16:51:54 +0200

    finalized cluster graphic and colors between netwoork plot and cluster plots

--- WP4/Policy_Interactions/R/06_networkstats.R
+++ WP4/Policy_Interactions/R/06_networkstats.R
@@ -8,6 +8,8 @@ library("dplyr")
 library("igraph")
 library("tibble")
 library("RColorBrewer")
+library("ggplot2")
+library("ggwordcloud")
 
 # Define functions --------------------------------------------------------
 
@@ -28,16 +30,16 @@ Q1.net2nd.graph<-readRDS("WP4/Policy_Interactions/data/03.Q1secondordercit.netwo
 
 # -- terms co-occurance -- #
 
-Q1.terms<-read.csv("WP4/Policy_Interactions/data/03.Q1term.edgelist.csv")              # edge list
-Q1.terms.meta<-read.csv("WP4/Policy_Interactions/data/03.Q1term.verticesmetadata.csv") # vertices meta data
-Q1.terms.graph<-readRDS("WP4/Policy_Interactions/data/03.Q1.termnetwork.rds")          # the network object 
+Q1.terms<-read.csv("WP4/Policy_Interactions/data/03.Q1term.edgelist.csv")                         # edge list
+Q1.terms.meta<-read.csv("WP4/Policy_Interactions/data/03.Q1term.verticesmetadata.csv")            # vertices meta data
+Q1.terms.graph<-readRDS("WP4/Policy_Interactions/data/03.Q1.termnetwork.rds")                     # the network object 
 
 # ----------- Query 2 (Q2) ---------- #
 
 # -- citations -- #
-Q2.net1st<-read.csv("WP4/Policy_Interactions/data/03.Q2firstordercit.edgelist.csv")              # edge list
-Q2.net1st.meta<-read.csv("WP4/Policy_Interactions/data/03.Q2firstordercit.verticesmetadata.csv") # vertices meta data
-Q2.net1st.graph<-readRDS("WP4/Policy_Interactions/data/03.Q2firstordercit.network.rds")          # the network object 
+Q2.net1st<-read.csv("WP4/Policy_Interactions/data/03.Q2firstordercit.edgelist.csv")               # edge list
+Q2.net1st.meta<-read.csv("WP4/Policy_Interactions/data/03.Q2firstordercit.verticesmetadata.csv")  # vertices meta data
+Q2.net1st.graph<-readRDS("WP4/Policy_Interactions/data/03.Q2firstordercit.network.rds")           # the network object 
 
 Q2.net2nd<-read.csv("WP4/Policy_Interactions/data/03.Q2secondordercit.edgelist.csv")              # edge list
 Q2.net2nd.meta<-read.csv("WP4/Policy_Interactions/data/03.Q2secondordercit.verticesmetadata.csv") # vertices meta data
@@ -45,9 +47,9 @@ Q2.net2nd.graph<-readRDS("WP4/Policy_Interactions/data/03.Q2secondordercit.netwo
 
 # -- terms co-occurance -- #
 
-Q2.terms<-read.csv("WP4/Policy_Interactions/data/03.Q2term.edgelist.csv")              # edge list
-Q2.terms.meta<-read.csv("WP4/Policy_Interactions/data/03.Q2term.verticesmetadata.csv") # vertices meta data
-Q2.terms.graph<-readRDS("WP4/Policy_Interactions/data/03.Q2.termnetwork.rds")          # the network object 
+Q2.terms<-read.csv("WP4/Policy_Interactions/data/03.Q2term.edgelist.csv")                         # edge list
+Q2.terms.meta<-read.csv("WP4/Policy_Interactions/data/03.Q2term.verticesmetadata.csv")            # vertices meta data
+Q2.terms.graph<-readRDS("WP4/Policy_Interactions/data/03.Q2.termnetwork.rds")                     # the network object 
 
 # Network Stats ---------------------------------------------------------------
 
@@ -222,20 +224,38 @@ colors3 <- c(colors)
 
 V(Q1.network.updated1)$color <- colors3[as.numeric(as.factor(V(Q1.network.updated1)$membership))]
 
-cbind(V(Q1.network.updated1)$color, V(Q1.network.updated1)$membership)
+degrees <- 
+    as.data.frame(cbind(degree(Q1.terms.graph))) %>%
+    rownames_to_column() %>%
+    rename("degree" = "V1")
 
 as.data.frame(cbind(V(Q1.network.updated1)$color, V(Q1.network.updated1)$membership)) %>%
     mutate(V2 = as.numeric(V2)) %>%
     distinct(., .keep_all=TRUE) %>%
     right_join(.,Q1.terms.meta2, by = c("V2" = "membership")) %>%
+    left_join(.,degrees, by = c("item1"= "rowname")) %>%
+    mutate(V2 =paste("Cluster", V2, sep = " ")) %>%
+    mutate(V2= as.factor(V2)) %>%
     ggplot(., aes(label = item1, 
-                  size = total.count,
-                  color = V1)) +
-    geom_text_wordcloud(shape = "circle") +
-    scale_size_area(max_size = 10) +
+                  size = degree,
+                 # x=V2,
+                  color = V2)) +
+    geom_text_wordcloud(shape = "circle", eccentricity = 1) +
+    scale_size_area(max_size = 12) +
     theme_minimal() +
-    facet_wrap(~V2)  
-xx
+    theme(strip.text.x = element_text(size = 15, face = "bold")) + 
+    scale_color_manual(breaks = c("Cluster 1", "Cluster 2", "Cluster 3",
+                                  "Cluster 4", "Cluster 5", "Cluster 6",
+                                  "Cluster 7"),
+                       values=c("#1B9E77", 
+                                "#D95F02", 
+                                "#7570B3", 
+                                "#E7298A", 
+                                "#66A61E", 
+                                "#E6AB02", 
+                                "#A6761D"))
+ggsave("WP4/Policy_Interactions/Results/Q1.termclusters.png")
+
 l3 <- layout.fruchterman.reingold(Q1.network.updated1)
 
 dist <- seq(-.025,0.25, by=.0024)
@@ -445,23 +465,6 @@ Q2.terms.meta2 <-
     Q2.terms.meta %>%
     left_join(.,member.attributes, by = c("item1"="rowname"))
 
-
-Q2.terms.meta2 %>%
-    filter(membership <10) %>%
-    ggplot(., aes(label = item1, size = total.count)) +
-    geom_text_wordcloud(shape = "circle") +
-    scale_size_area(max_size = 8) +
-    theme_minimal() +
-    facet_wrap(~membership)  
-
-Q2.terms.meta2 %>%
-    filter(membership >10) %>%
-    ggplot(., aes(label = item1, size = total.count)) +
-    geom_text_wordcloud(shape = "circle") +
-    scale_size_area(max_size = 8) +
-    theme_minimal() +
-    facet_wrap(~membership)  
-
 network.updated <- graph_from_data_frame(d=Q2.terms, vertices = Q2.terms.meta2, directed = FALSE)
 class(network3)
 
@@ -474,10 +477,74 @@ n_distinct(V(network.updated1)$membership)
 library(RColorBrewer)
 
 colors <- brewer.pal(n = 8, name = "Dark2")
-colors2 <- brewer.pal(n = 8, name = "Set1")
-colors3 <- c(colors, colors2)
+colors2 <- brewer.pal(n = 10, name = "Paired")
+colors2.5 <- colors2[-4] # remove the unreadable yellow
+colors2.5 <- colors2.5[-6] # remove the unreadable yellow
+
+colors3 <- c(colors, colors2.5)
 
 V(network.updated1)$color <- colors3[as.numeric(as.factor(V(network.updated1)$membership))]
+
+# clusters not in the network... 2, 3, 15, 17, 19 but will still need a color that is not present in the network for the word clouds 
+
+degrees1 <- 
+    as.data.frame(cbind(degree(Q2.terms.graph))) %>%
+    rownames_to_column() %>%
+    rename("degree" = "V1")
+
+as.data.frame(cbind(V(network.updated1)$color, V(network.updated1)$membership)) %>%
+    mutate(V2 = as.numeric(V2)) %>%
+    distinct(., .keep_all=TRUE) %>%
+    right_join(.,Q2.terms.meta2, by = c("V2" = "membership")) %>%
+    left_join(.,degrees1, by = c("item1"= "rowname")) %>%
+    mutate(V22 =paste("Cluster", V2, sep = " ")) %>%
+    mutate(V22= factor(V22, levels = c("Cluster 1", "Cluster 2", "Cluster 3",
+                                     "Cluster 4", "Cluster 5", "Cluster 6",
+                                     "Cluster 7","Cluster 8","Cluster 9",
+                                     "Cluster 10","Cluster 11", "Cluster 12", "Cluster 13",
+                                     "Cluster 14", "Cluster 15", "Cluster 16",
+                                     "Cluster 17","Cluster 18","Cluster 19",
+                                     "Cluster 20", "Cluster 21"))) %>%
+    ggplot(., aes(label = item1, 
+                  size = total.count,
+                  color = V22)) +
+    geom_text_wordcloud(shape = "circle",eccentricity = 1) +
+    scale_size_area(max_size = 50) +
+    theme_minimal() +
+    theme(line = element_blank(),
+          text = element_blank(),
+          title = element_blank()) + 
+    scale_color_manual(breaks = c("Cluster 1", "Cluster 2","Cluster 3",
+                                  "Cluster 4", "Cluster 5", "Cluster 6",
+                                  "Cluster 7","Cluster 8","Cluster 9",
+                                  "Cluster 10","Cluster 11", "Cluster 12", "Cluster 13",
+                                  "Cluster 14", "Cluster 15", "Cluster 16",
+                                  "Cluster 17", "Cluster 18","Cluster 19",
+                                  "Cluster 20", "Cluster 21"),
+                       values=c("#1B9E77", 
+                                "black",
+                                "purple",
+                                "#D95F02", 
+                                "#7570B3", 
+                                "#E7298A", 
+                                "#66A61E",
+                                "#E6AB02",
+                                "#A6761D",
+                                "#666666",
+                                "#A6CEE3", 
+                                "#1F78B4", 
+                                "#B2DF8A", 
+                                "#FB9A99",
+                                "#ff0099",
+                                "#E31A1C",
+                                "#0000ff",
+                                "#FF7F00",
+                                "00cccc",
+                                "#CAB2D6",
+                                "#6A3D9A"))
+
+ggsave("WP4/Policy_Interactions/Results/Q2.termclusters2.png",
+       width = 32, height = 30)
 
 l3 <- layout.fruchterman.reingold(network.updated1)
 
