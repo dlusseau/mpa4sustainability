34d6589eef8e8766ccc0063f32cc8d4f00f28774
Anna Elizabeth Jorgensen <aeljor@dtu.dk>
2022-10-21 15:24:44 +0200

    obtaining the text for the whole network

--- WP4/Policy_Interactions/R/08_predictiontext.R
+++ WP4/Policy_Interactions/R/08_predictiontext.R
@@ -4,13 +4,14 @@
 # Clear work space ---------------------------------------------------------
 rm(list = ls())
 Sys.setenv(LANG = "en") # change the language to english 
-
+gc()
 # Load libraries ----------------------------------------------------------
 
 library("dplyr")
 library("eurlex")
 library("tibble")
 library("purrr")
+library("igraph")
 
 # Load data ---------------------------------------------------------------
 
@@ -38,7 +39,9 @@ MPA.DESG.text <- read.csv(file = "WP4/Policy_Interactions/data/01_mpaterms.text.
 
 # Query one ---------------------
 
-# check with "to" documents we already have text from (using Q2 text data since it contains all for Q1 pulls and more)
+# Citation text --------
+
+# check which "to" documents we already have text from (using Q2 text data since it contains all for Q1 pulls and more)
 MPA.DESG.text.cut <-
   MPA.DESG.text %>%
   select(CELEX,total.text) %>%
@@ -52,21 +55,25 @@ toQ1.textdf <-
   left_join(.,MPA.DESG.text.cut, by = c("to"="CELEX")) %>%
   distinct(., .keep_all = TRUE) %>% # since some cite multiple documents. 
   rename("CELEX" = "to")
-
+# 24
 toQ1.NOtextdf <- 
   Q1.net2nd %>%
   select(to) %>%
   filter(! to %in% MPA.DESG.text.cut$CELEX) %>%
   distinct(., .keep_all = TRUE) %>% # since some cite multiple documents. 
   rename("CELEX" = "to")
+# 546
+# 24 + 546 = 570
 
-# 90 + 28 = 118
 Q1.net2nd %>%
   select(to) %>%
-  distinct(., .keep_all = TRUE) # since some cite multiple documents. 
-# math checks out there is 118 observations (celex)
+  distinct(., .keep_all = TRUE)  
+# math checks out there is 570 citations celex
 
-#from of the edge df 
+# Citer text ----------
+
+# "from" column of the edge df 
+
 fromQ1.textdf <- 
   Q1.net2nd %>%
   select(from) %>%
@@ -74,31 +81,37 @@ fromQ1.textdf <-
   left_join(.,MPA.DESG.text.cut, by = c("from"="CELEX")) %>%
   distinct(., .keep_all = TRUE) %>% # since some cite multiple documents. 
   rename("CELEX" = "from")
-  
+# 28
+
 fromQ1.NOtextdf <- 
   Q1.net2nd %>%
   select(from) %>%
   filter(! from %in% MPA.DESG.text.cut$CELEX) %>%
   distinct(., .keep_all = TRUE) %>% # since some cite multiple documents. 
   rename("CELEX" = "from")
+# 90
 
-# 24 + 546 = 570
+# 28 + 90 = 118
+
 Q1.net2nd %>%
   select(from) %>%
-  distinct(., .keep_all = TRUE) # since some cite multiple documents. 
-# math checks out there is 570 observations (celex)
+  distinct(., .keep_all = TRUE) 
+# math checks out there is 118 citers (celex)
 
 # join the to and from columns and remove duplicates
 Q1edge.text <-
   rbind(fromQ1.textdf, toQ1.textdf) %>%
-  distinct(., .keep_all = TRUE) # 37 celex
+  distinct(., .keep_all = TRUE) # 37 celex text
 
 gc() # clear up some space before the text pulling
 
-Q1edge.NOtext <- 
+Q1edge.NOtextsec1 <- 
   rbind(fromQ1.NOtextdf, toQ1.NOtextdf) %>%
   distinct(., .keep_all = TRUE) %>% # 546 celex
+  slice(0:273) %>% # 273
   mutate(url = paste0("http://publications.europa.eu/resource/celex/",.$CELEX)) %>% 
+  mutate(title = map_chr(url, elx_fetch_data, "title")) %>% 
+  as_tibble() %>%
   mutate(title = map_chr(url, elx_fetch_data, "title")) %>% 
   as_tibble() %>%
   mutate(text = map_chr(url, elx_fetch_data, "text")) %>% 
@@ -106,10 +119,32 @@ Q1edge.NOtext <-
   mutate(total.text = paste0(.$title,.$text)) %>%
   select(CELEX, total.text)
 
-Q1.edge.text <-
-  rbind(Q1edge.text,Q1edge.NOtext) #546 + 37 = 583 --> all checks out this is the total number of vertices in the network 
+gc() # clear up some space before the text pulling
 
+Q1edge.NOtextsec2 <- 
+  rbind(fromQ1.NOtextdf, toQ1.NOtextdf) %>%
+  distinct(., .keep_all = TRUE) %>% # 546 celex
+  slice(274:546) %>% # 515
+  mutate(url = paste0("http://publications.europa.eu/resource/celex/",.$CELEX)) %>% 
+  mutate(title = map_chr(url, elx_fetch_data, "title")) %>% 
+  as_tibble() %>%
+  mutate(title = map_chr(url, elx_fetch_data, "title")) %>% 
+  as_tibble() %>%
+  mutate(text = map_chr(url, elx_fetch_data, "text")) %>% 
+  as_tibble() %>%
+  mutate(total.text = paste0(.$title,.$text)) %>%
+  select(CELEX, total.text)
+
+
+Q1.edge.text <-
+  rbind(Q1edge.text,Q1edge.NOtextsec1,Q1edge.NOtextsec2) #546 + 37 = 583 --> all checks out this is the total number of vertices in the network 
+
+V(Q1.net2nd.graph) # 583 vertices
+edges <- degree(Q1.net2nd.graph)
+sum(edges)
 # Query two ---------------------
+
+# Citation text --------
 
 # check with "to" documents we already have text from
 
@@ -121,19 +156,24 @@ toQ2.textdf <-
   left_join(.,MPA.DESG.text.cut, by = c("to"="CELEX")) %>%
   distinct(., .keep_all = TRUE) %>% # since some cite multiple documents. 
   rename("CELEX" = "to")
-
+# 36
 toQ2.NOtextdf <- 
   Q2.net2nd %>%
   select(to) %>%
   filter(! to %in% MPA.DESG.text.cut$CELEX) %>%
   distinct(., .keep_all = TRUE) %>% # since some cite multiple documents. 
   rename("CELEX" = "to")
+# 1029
 
-# 85 + 227 = 312
+# 36 + 1029 = 1065
+
 Q2.net2nd %>%
   select(to) %>%
-  distinct(., .keep_all = TRUE) # since some cite multiple documents. 
-# math checks out there is 312 observations (celex)
+  distinct(., .keep_all = TRUE) %>% # since some cite multiple documents. 
+  dim()
+# math checks out there is 1065 observations (celex)
+
+# Citer text ----------
 
 #from of the edge df 
 fromQ2.textdf <- 
@@ -143,6 +183,7 @@ fromQ2.textdf <-
   left_join(.,MPA.DESG.text.cut, by = c("from"="CELEX")) %>%
   distinct(., .keep_all = TRUE) %>% # since some cite multiple documents. 
   rename("CELEX" = "from")
+# 85
 
 fromQ2.NOtextdf <- 
   Q2.net2nd %>%
@@ -150,13 +191,15 @@ fromQ2.NOtextdf <-
   filter(! from %in% MPA.DESG.text.cut$CELEX) %>%
   distinct(., .keep_all = TRUE) %>% # since some cite multiple documents. 
   rename("CELEX" = "from")
+# 227
 
-# 36 + 1029 = 1065
+# 85 + 227 = 312
+
 Q2.net2nd %>%
   select(from) %>%
   distinct(., .keep_all = TRUE) %>% # since some cite multiple documents. 
   dim()
-# math checks out there is 1065 observations (celex)
+# math checks out there is 312 observations (celex)
 
 
 # join the to and from columns and remove duplicates
@@ -171,7 +214,7 @@ Q2edge.NOtext.sec1 <-
   rbind(fromQ2.NOtextdf, toQ2.NOtextdf) %>%
   distinct(., .keep_all = TRUE) %>% # 1029 celex
   mutate(url = paste0("http://publications.europa.eu/resource/celex/",.$CELEX)) %>%
-  slice(1:514) %>% # 514
+  slice(1:343) %>% # 343
   mutate(title = map_chr(url, elx_fetch_data, "title")) %>% 
   as_tibble() %>%
   mutate(text = map_chr(url, elx_fetch_data, "text")) %>% 
@@ -185,7 +228,20 @@ Q2edge.NOtext.sec2 <-
   rbind(fromQ2.NOtextdf, toQ2.NOtextdf) %>%
   distinct(., .keep_all = TRUE) %>% # 1029 celex
   mutate(url = paste0("http://publications.europa.eu/resource/celex/",.$CELEX)) %>%
-  slice(515:1029) %>% # 515
+  slice(344:686) %>% # 343
+  mutate(title = map_chr(url, elx_fetch_data, "title")) %>% 
+  as_tibble() %>%
+  mutate(text = map_chr(url, elx_fetch_data, "text")) %>% 
+  as_tibble() %>%
+  mutate(total.text = paste0(.$title,.$text)) %>%
+  select(CELEX, total.text)
+
+
+Q2edge.NOtext.sec3 <- 
+  rbind(fromQ2.NOtextdf, toQ2.NOtextdf) %>%
+  distinct(., .keep_all = TRUE) %>% # 1029 celex
+  mutate(url = paste0("http://publications.europa.eu/resource/celex/",.$CELEX)) %>%
+  slice(687:1029) %>% # 343
   mutate(title = map_chr(url, elx_fetch_data, "title")) %>% 
   as_tibble() %>%
   mutate(text = map_chr(url, elx_fetch_data, "text")) %>% 
@@ -194,8 +250,11 @@ Q2edge.NOtext.sec2 <-
   select(CELEX, total.text)
   
 Q2.edge.text <-
-  rbind(Q2edge.text,Q2edge.NOtext.sec1,Q2edge.NOtext.sec2) #515 + 514 + 97 = 1126 --> all checks out this is the total number of vertices in the network 
+  rbind(Q2edge.text,Q2edge.NOtext.sec1,Q2edge.NOtext.sec2,Q2edge.NOtext.sec3) #1029 + 97 = 1126 --> all checks out this is the total number of vertices in the network 
 
+V(Q2.net2nd.graph) # 1126 vertices
+edges <- degree(Q2.net2nd.graph)
+sum(edges)
 # Save ---------------------------------------------------------------
 
 write.csv(Q2.edge.text, file = "WP4/Policy_Interactions/data/08.Q2C2.edge.text.csv", row.names=FALSE)
