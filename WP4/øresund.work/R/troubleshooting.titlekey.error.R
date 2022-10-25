17e7ce5a3a4da3c4f03eab78494d677c03c28834
Anna Elizabeth Jorgensen <aeljor@dtu.dk>
2022-10-25 15:30:47 +0200

    using a way to work around the http 500 error. finish directives need to do the other types of legislation.

--- WP4/øresund.work/R/troubleshooting.titlekey.error.R
+++ WP4/øresund.work/R/troubleshooting.titlekey.error.R
@@ -10,6 +10,7 @@ library("dplyr")
 library("eurlex")
 library("purrr")
 
+# Build a legislation-title key -------------------------------------------------
 
 # EURLEX KEY
 document.key.df <- read.csv(file = "C:/Users/aeljor/OneDrive - Danmarks Tekniske Universitet/Skrivebord/mpa4sustainability/WP4/Policy_Interactions/data/01_SPARQL.key.df.csv")
@@ -22,6 +23,9 @@ document.key.df1 <-
 
 rm(document.key.df) # remove this bc it takes up a lot of space
 
+# --- Directives --- #
+
+# run on 25-10-2022
 directive.titles <- 
   document.key.df1 %>%
   filter(resource.type == "DIR") %>%
@@ -29,41 +33,57 @@ directive.titles <-
   filter(!is.na(.$celex)) %>% # remove na values for celex
   select(-resource.type,-celex)
 
-rm(document.key.df1) # remove this bc it takes up a lot of space
-
-directive.titles[2652,]
-directive.titles[2480,]
-
-
-test<-array(0)
-gc()
-
-for (i in seq(dim(directive.titles)[1])) {
-
+test<-array(0) #2659, 4080 problem ones, skip these and do them later (see below)
+for (i in 4081:dim(directive.titles)[1]) {
   test[i]<-elx_fetch_data(directive.titles$work[i],type="title")
-  
   print(i)
-  
   flush.console()
-  gc()
-  
 }
+
+i<-2659
+test[i]<-elx_fetch_data(directive.titles$work[i],type="title")
+# ran this above
+i<-4080
+test[i]<-elx_fetch_data(directive.titles$work[i],type="title")
+
+directive.titles$titles<-test
+
+write.csv(directive.titles, file = "C:/Users/aeljor/OneDrive - Danmarks Tekniske Universitet/Skrivebord/mpa4sustainability/WP4/øresund.work/data/03.EurLexKey.directive.titles.csv") 
+            
+# --- Decisions --- #
 
 decision.titles <- 
   document.key.df1 %>%
   filter(resource.type == "DEC") %>%
-  mutate(title = map_chr(work, elx_fetch_data, "title")) %>% 
-  select(celex,title) %>%
-  mutate(celex = as.factor(celex))
+  distinct(celex, .keep_all = TRUE) %>%
+  filter(!is.na(.$celex)) %>% # remove na values for celex
+  select(-resource.type,-celex)
+
+dec.test<-array(0) #2652 3402 3469
+for (i in 1:dim(decision.titles)[1]) {
+  dec.test[i]<-elx_fetch_data(decision.titles$work[i],type="title")
+  print(i)
+  flush.console()
+}
+
+
+# --- Regulation --- #
 
 regulation.titles <- 
   document.key.df1 %>%
-  filter(resource.type == "REG") %>%
-  mutate(title = map_chr(url, elx_fetch_data, "title")) %>% 
-  select(celex,title) %>%
-  mutate(celex = as.factor(celex)) 
+  filter(resource.type == "REG")%>%
+  distinct(celex, .keep_all = TRUE) %>%
+  filter(!is.na(.$celex)) %>% # remove na values for celex
+  select(-resource.type,-celex)
 
-# archived ---------------------------
+
+
+
+
+
+
+
+# Archived ---------------------------
 
 gc()
 test <- 
@@ -93,5 +113,37 @@ for (i in seq(celex)) {
 # keep getting: 
 # Internal Server Error (HTTP 500).Error in UseMethod("status_code") : 
 # no applicable method for 'status_code' applied to an object of class "NULL"
+
+
+
+rm(document.key.df1) # remove this bc it takes up a lot of space
+
+directive.titles[2652,]
+directive.titles[2480,]
+
+
+test<-array(0)
+gc()
+
+for (i in seq(dim(directive.titles)[1])) {
+  
+  test[i]<-elx_fetch_data(directive.titles$work[i],type="title")
+  
+  print(i)
+  
+  flush.console()
+  gc()
+  
+}
+
+
+regulation.titles <- 
+  document.key.df1 %>%
+  filter(resource.type == "REG") %>%
+  mutate(title = map_chr(url, elx_fetch_data, "title")) %>% 
+  select(celex,title) %>%
+  mutate(celex = as.factor(celex)) 
+
+
 
 
