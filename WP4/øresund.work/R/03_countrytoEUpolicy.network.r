dd548c64d92157a481d1a85a8eddd823de6d0a10
Anna Elizabeth Jorgensen <aeljor@dtu.dk>
2022-10-27 16:32:29 +0200

    adding network stas and making the network plot nicer

--- WP4/øresund.work/R/03_countrytoEUpolicy.network.r
+++ WP4/øresund.work/R/03_countrytoEUpolicy.network.r
@@ -61,10 +61,104 @@ V(network)$color <- col[as.numeric(as.factor(V(network)$source))]
 # DK doc are the red ones...
 degree <- degree(network)
 
+median(degree)
+#2
+quantile(degree,probs = c(0,.25,.5,.75,.95,1))
+#  0%   25%   50%   75%   95%  100% 
+#1.00  1.00  2.00  3.00  7.05 82.00 
+
 l <- layout.fruchterman.reingold(network)
+
 plot(network,
-     vertex.label=NA,
+     vertex.label=ifelse(degree(network) >7.05 & V(network)$source == "EU",
+                         V(network)$name,NA),
+     vertex.label.cex = .75,
+     vertex.size= 3,
+     layout = l)
+#labels are those EU docs that are > the 95% percentile for the degree number
+
+# network stats ----------------------------------
+
+# degree 
+network.degree<-degree(network)
+
+summary(network.degree)
+
+
+# betweenness
+network.betweenness<-betweenness(network, directed = FALSE, normalized=TRUE)
+
+summary(network.betweenness)
+
+plot(network.degree ~ network.betweenness)
+
+# term clusters/groups
+# cluster_leading_eigen: 
+
+#   "Community structure detecting based on the leading eigenvector of the community matrix" 
+#   "This function tries to find densely connected subgraphs in a graph by calculating the leading nonnegative 
+#    eigenvector of the modularity matrix of the graph." - CRAN PDF
+E(network)
+
+network.clusters <-cluster_leading_eigen(network,
+                                       # steps = -1,
+                                        weights = NULL)
+sort(table(network.clusters$membership))
+# cluster 33 is the largest!
+
+plot_dendrogram(network.clusters)
+
+l2 <- layout.fruchterman.reingold(network)
+
+plot(network.clusters, network, 
+     #vertex.label.color="black",
+     vertex.shape="circle",
+     vertex.label = NA,
+     # vertex.label.cex=V(Q1.terms.graph)$total.count*.05,
+    # edge.width=E(network.clusters)$n*1,
+     rescale = TRUE,
+     ylim=c(-.8,.85),xlim=c(-.9,.9),
+     vertex.size=3,
+     layout=l2
+)
+
+install.packages("viridis")  # Install
+library("viridis")           # Load
+require(graphics)
+
+# solution to the color issue here:
+# https://statisticsglobe.com/create-distinct-color-palette-in-r
+palette3_info <- brewer.pal.info[brewer.pal.info$category == "qual", ]  # Extract color info
+palette3_all <- unlist(mapply(brewer.pal,                     # Create vector with all colors
+                              palette3_info$maxcolors,
+                              rownames(palette3_info)))
+palette3_all   
+
+set.seed(1)                                             # Set random seed
+palette3 <- sample(palette3_all, 42)                    # Sample colors
+palette3 
+
+V(network)$color <- palette3[as.numeric(as.factor(membership(network.clusters)))]
+
+
+plot(network,
+     vertex.color=V(network)$color,
+     vertex.label=ifelse(degree(network) >7.05 & V(network)$source == "EU",
+                         V(network)$name,NA),
+     vertex.label.cex = .75,
+     vertex.shape = ifelse(V(network)$source == "EU",
+                           "circle","square"),
+     vertex.size= 3,
+     layout = l2)
+
+plot(network,
+     vertex.label=ifelse(degree(network) >7.05 & V(network)$source == "EU",
+                         V(network)$name,NA),
+     vertex.label.cex = .75,
      vertex.size= 2,
      layout = l)
 
-sort(degree(network), decreasing = TRUE)
+
+
+
+
