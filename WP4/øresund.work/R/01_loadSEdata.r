a2aac43623058bbf82fb0d4f7c1916de4cdec0da
Anna Elizabeth Jorgensen <aeljor@dtu.dk>
2023-02-13 10:34:41 +0100

    reverting back to the original API which was saved and used: not an exact match and "" were used. The exact match script did not save the files and the files we did use was the prev. script code. Also changed the comments to be clearer and concise

--- WP4/øresund.work/R/01_loadSEdata.r
+++ WP4/øresund.work/R/01_loadSEdata.r
@@ -3,7 +3,6 @@
 rm(list = ls())
 Sys.setenv(LANG = "en") # change the language to english 
 
-# Define functions -------------------------------------------------------------
 
 # Load data --------------------------------------------------------------------
 
@@ -16,19 +15,15 @@ library("gtools")
 library("plyr")
 library("dplyr")
 
-# Using the Swedish Parlaments open data website. Has an API that searches for Svensk författningssamling (SFS). 
-# from all the digginig in I have done. I think this is the exact same thing as what is stated on https://lagrummet.se/lagrummet/rattsinformation/lagar-och-forordningar
-
 # Made the api queries from there builder:
 # https://data.riksdagen.se/dokumentlista/
-
+# but we needed to further adjust it so it ali included the https://www.riksdagen.se/sv/Dokument-Lagar/ filter of Document status which wasnt an option in the builder but we also put it in 
 # filter set to have the search word, document type is Svensk författningssamling (SFS) the output was clicked to be in JSON format
   
 # "fiske" query ------------------------------------------------------------------
 
 Fiske.API.URL <-
-#"https://data.riksdagen.se/dokumentlista/?sok=%22fiske%22&doktyp=SFS&rm=&from=&tom=&ts=&bet=&tempbet=&nr=&org=&iid=&avd=&webbtv=&talare=&exakt=&planering=&facets=&sort=rel&sortorder=desc&rapport=&utformat=json&a=s#soktraff"
-"https://data.riksdagen.se/dokumentlista/?sok=fiske&dokstat=g%C3%A4llande+sfs&doktyp=SFS&rm=&from=&tom=&ts=&bet=&tempbet=&nr=&org=&iid=&avd=&webbtv=&talare=&exakt=1&planering=&facets=&sort=rel&sortorder=desc&rapport=&utformat=json&a=s#soktraff"
+ "https://data.riksdagen.se/dokumentlista/?sok=%22fiske%22&dokstat=g%C3%A4llande+sfs&doktyp=SFS&rm=&from=&tom=&ts=&bet=&tempbet=&nr=&org=&iid=&avd=&webbtv=&talare=&exakt=&planering=&facets=&sort=rel&sortorder=desc&rapport=&utformat=json&a=s#soktraff"
 
 fiske.raw.data <- GET(Fiske.API.URL)
 fiske.raw.data$status_code # 200 means it is ok :)
@@ -78,12 +73,11 @@ print(i)
 
 ## ---------- now get the text from the url given in the df ---------- #
 
-# this works nicely... just need to make a loop with all the data like we did for dk text
+# need to make a loop with all the data like we did for dk text
 
-#found these problem urls yesterday Oct. 20th. two of them the reason is the space value changes once in the web but the other is just faulty 
+# found these problem urls yesterday Oct. 20th. one of them the reason is the space value changes once in the web but the other is just faulty 
 # for now I will remove the faulty text url and edit the other two so the work. 
 
-#https://data.riksdagen.se/dokument/sfs-1723-1016 1.text after I copy this into chrome it changes to: https://data.riksdagen.se/dokument/sfs-1723-1016%201.text
 #https://data.riksdagen.se/dokument/sfs-1736-0123 1.text after I copy this into chrome it changes to: https://data.riksdagen.se/dokument/sfs-1736-0123%201 # for both the space is replaced with %20
 #https://data.riksdagen.se/dokument/sfs-1910-72 s.1.text --> this is just a faulty link in the online search it comes up as a result but says no document is found...
 
@@ -92,19 +86,22 @@ full.fisk.df %>%
   mutate(detect = str_detect(dokument_url_text, " ")) %>%
   select(detect,dokument_url_text)%>%
   filter(detect == TRUE)
+#detect                                 dokument_url_text
+#   TRUE //data.riksdagen.se/dokument/sfs-1736-0123 1.text
+#   TRUE //data.riksdagen.se/dokument/sfs-1910-72 s.1.text 
 
 # totally remove the faulty link and then edit the two to be the correct path
 full.fisk.df1 <- 
   full.fisk.df %>%
   filter(dokument_url_text != "//data.riksdagen.se/dokument/sfs-1910-72 s.1.text") %>%
   mutate(dokument_url_text = str_replace_all(dokument_url_text, " ", "%20"))
-  
+
 URLs <- paste0("https:",full.fisk.df1[,18], sep="") #put https: infront of the url
 
 doc.id <- full.fisk.df1[,16]
 
 
-SK.fisketext.list <- structure(vector("list", 84), names=doc.id)
+SK.fisketext.list <- structure(vector("list", 113), names=doc.id)
 
 for (i in seq(URLs)) {
   
@@ -122,8 +119,7 @@ for (i in seq(URLs)) {
 # jakt query -----------
 
 jakt.API.URL <-
-#"https://data.riksdagen.se/dokumentlista/?sok=%22jakt%22&doktyp=SFS&rm=&from=&tom=&ts=&bet=&tempbet=&nr=&org=&iid=&avd=&webbtv=&talare=&exakt=&planering=&facets=&sort=rel&sortorder=desc&rapport=&utformat=json&a=s#soktraff"
-"https://data.riksdagen.se/dokumentlista/?sok=jakt&dokstat=g%C3%A4llande+sfs&doktyp=SFS&rm=&from=&tom=&ts=&bet=&tempbet=&nr=&org=&iid=&avd=&webbtv=&talare=&exakt=1&planering=&facets=&sort=rel&sortorder=desc&rapport=&utformat=json&a=s#soktraff"
+  "https://data.riksdagen.se/dokumentlista/?sok=%22jakt%22&dokstat=g%C3%A4llande+sfs&doktyp=SFS&rm=&from=&tom=&ts=&bet=&tempbet=&nr=&org=&iid=&avd=&webbtv=&talare=&exakt=&planering=&facets=&sort=rel&sortorder=desc&rapport=&utformat=json&a=s#soktraff"
 
 jakt.raw.data <- GET(jakt.API.URL)
 jakt.raw.data$status_code # 200 means it is ok :)
@@ -136,9 +132,6 @@ jakt.data.list <- stri_encode(as.raw(jakt.raw.data$content), 'UTF-8') #data_list
 jakt.data.object <- fromJSON(jakt.data.list)
 
 jakt.df <- jakt.data.object$dokumentlista$dokument
-
-# this is only the first page.... need to figure out how to get the other pages each page as 20 results.
-# someone else has this Q on stackoverflow: https://stackoverflow.com/questions/54575231/a-continuation-of-extracting-data-from-an-api-using-r 
 
 No.pgs <- as.numeric(jakt.data.object[["dokumentlista"]][["@sidor"]])
 
@@ -174,8 +167,6 @@ for (i in seq(No.pgs)) {
 
 ## ---------- now get the text from the url given in the df ---------- #
 
-# this works nicely... just need to make a loop with all the data like we did for dk text
-
 doc.id <- full.jakt.df[,16]
 
 SK.jakttext.list <- structure(vector("list", 35), names=doc.id)
@@ -199,8 +190,9 @@ for (i in seq(URLs)) {
 # sjöfart query -----------
 
 sjofart.API.URL <-
-#"https://data.riksdagen.se/dokumentlista/?sok=%22sj%C3%B6fart%22&doktyp=SFS&rm=&from=&tom=&ts=&bet=&tempbet=&nr=&org=&iid=&avd=&webbtv=&talare=&exakt=&planering=&facets=&sort=rel&sortorder=desc&rapport=&utformat=json&a=s#soktraff"
-"https://data.riksdagen.se/dokumentlista/?sok=sj%C3%B6fart&dokstat=g%C3%A4llande+sfs&doktyp=SFS&rm=&from=&tom=&ts=&bet=&tempbet=&nr=&org=&iid=&avd=&webbtv=&talare=&exakt=1&planering=&facets=&sort=rel&sortorder=desc&rapport=&utformat=json&a=s#soktraff"
+  "https://data.riksdagen.se/dokumentlista/?sok=%22sj%C3%B6fart%22&dokstat=g%C3%A4llande+sfs&doktyp=SFS&rm=&from=&tom=&ts=&bet=&tempbet=&nr=&org=&iid=&avd=&webbtv=&talare=&exakt=&planering=&facets=&sort=rel&sortorder=desc&rapport=&utformat=json&a=s#soktraff"
+
+  
 sjofart.raw.data <- GET(sjofart.API.URL)
 sjofart.raw.data$status_code # 200 means it is ok :)
 names(sjofart.raw.data)
@@ -212,9 +204,6 @@ sjofart.data.list <- stri_encode(as.raw(sjofart.raw.data$content), 'UTF-8') #dat
 sjofart.data.object <- fromJSON(sjofart.data.list)
 
 sjofart.df <- sjofart.data.object$dokumentlista$dokument
-
-# this is only the first page.... need to figure out how to get the other pages each page as 20 results.
-# someone else has this Q on stackoverflow: https://stackoverflow.com/questions/54575231/a-continuation-of-extracting-data-from-an-api-using-r 
 
 No.pgs <- as.numeric(sjofart.data.object[["dokumentlista"]][["@sidor"]])
 
@@ -250,22 +239,19 @@ for (i in seq(No.pgs)) {
 
 ## ---------- now get the text from the url given in the df ---------- #
 
-# this works nicely... just need to make a loop with all the data like we did for dk text
-#just to check that these are the only url with spaces... and they are
+#just to check that there are urls with spaces
+
 full.sjofart.df %>%
   mutate(detect = str_detect(dokument_url_text, " ")) %>%
   select(detect,dokument_url_text)%>%
   filter(detect == TRUE)
-# https://data.riksdagen.se/dokument/sfs-1891-35 s.1.text --> THIS LINK IS FAULTY
-# totally remove the faulty link and then edit the two to be the correct path
+
 full.sjofart.df1 <- 
-  full.sjofart.df  %>%
-  mutate(dokument_url_text = str_replace_all(dokument_url_text, " ", "%20"))
- #now not in our df since we are updating API #filter(dokument_url_text != "//data.riksdagen.se/dokument/sfs-1891-35 s.1.text")
+  full.sjofart.df  
 
 doc.id <- full.sjofart.df1[,16]
 
-SK.sjofart.list <- structure(vector("list", 108), names=doc.id)
+SK.sjofart.list <- structure(vector("list", 126), names=doc.id)
 
 URLs <- paste0("https:",full.sjofart.df1[,18], sep="") #put https: infront of the url
 
